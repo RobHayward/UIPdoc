961e5eef001333152e30385588800725c5308673
RobHayward <robohay@gmail.com>
2013-08-30 09:54:11 +0100

    Updated

--- CMchart.R
+++ CMchart.R
@@ -5,16 +5,29 @@ pdf("hist3.pdf", paper= "a4r", width = 9, title = "Carry trade")
 par(mfcol=c(2,3), oma = c(0,0,2,0))
 plot(density(dah$PNOKUSD, na.rm = TRUE), main = "NOK carry in Crisis", 
      col = 'red', lwd= 2, xlim = c(0.85, 1.10))
+abline(v = mean(dah$PNOKUSD))
+text(x = .95, y = 13, labels = "Mean")
 plot(density(dal$PNOKUSD, na.rm = TRUE), main = "NOK carry in Moderation", 
      col = 'red',lwd = 2, xlim = c(0.85, 1.10))
+abline(v = mean(dah$PNOKUSD))
+text(x = .95, y = 12, labels = "Mean")
 plot(density(dah$PCZKUSD, na.rm = TRUE), main = "CZK carry in Crisis", 
      col = 'red', lwd= 2, xlim = c(0.85, 1.10))
+abline(v = mean(dah$PCZKUSD))
+text(x = .94, y = 7, labels = "Mean")
 plot(density(dal$PCZKUSD, na.rm = TRUE), main = "CZK carry in Moderation", 
      col = 'red',lwd = 2, xlim = c(0.85, 1.10))
+abline(v = mean(dal$PCZKUSD, na.rm = TRUE))
+text(x = .92, y = 10, labels = "Mean")
+text(mean(dal$pCZKUSD))
 plot(density(dah$PRONUSD, na.rm = TRUE), main = "RON carry in Crisis", 
      col = 'red', lwd= 2, xlim = c(0.80, 1.10))
+abline(v = mean(dah$PRONUSD, na.rm = TRUE))
+text(x = .93, y = 8, labels = "Mean")
 plot(density(dal$PRONUSD, na.rm = TRUE), main = "RON carry in Moderation", 
      col = 'red',lwd = 2, xlim = c(0.85, 1.10))
+abline(v = mean(dal$PRONUSD, na.rm = TRUE))
+text(x = .96, y = 12, labels = "Mean")
 mtext("USD Carry trade returns in Crisis and Moderation", outer = TRUE)
 dev.off()
 # second hist graph----------------------
--- WRS.R
+++ WRS.R
@@ -1,5 +1,6 @@
 # Section{Comparison of the quantiles of the distributions---------
 require(WRS)
-qcomhd(dah$PNOKUSD, dal$PNOKUSD, q = seq(0.1, 0.9, by = 0.1))c
+qcomhd(dah$PNOKUSD, dal$PNOKUSD, q = seq(0.1, 0.9, by = 0.1))
 qcomhd(dah$PCZKUSD, dal$PCZKUSD, q = seq(0.1, 0.9, by = 0.1))
 qcomhd(dah$PRONUSD, dal$PRONUSD, q = seq(0.1, 0.9, by = 0.1))
+
